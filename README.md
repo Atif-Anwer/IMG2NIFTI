8885c589d36f6b844dafcc3b6e937621c3149df3
Atif <atif.anwer@insa-rouen.fr>
2023-01-09 11:14:56 +0100

    Added requirements.txt and readme.

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# IMG2NIFTI
+<a name="readme"></a>
+
+<!-- [![Contributors][contributors-shield]][contributors-url] -->
+![Python][python-shield]
+<!-- [![Stargazers][stars-shield]][stars-url] -->
+
+<!-- GETTING STARTED -->
+## Introduction
+
+Python script to convert Zeiss .img files to NIFTI format.
+- The script requires thepath to a root folder containing the *.img image files.
+- The converted nifti files (*.ni.gz) are saved in the same folder as the path provided.
+- The File names of the coverted nifti files are the same as the original img files.
+- Optionally, the script can also save individual slices in separate folders created for each *.img file
+- A mp4 video of the img file can also be saved if required
+- Optionally, random 50 slices in the image can also be viewed.
+
+## Requirements
+- pip
+- natsort>=8.2.0
+- nibabel>=4.0.2
+- numpy>=1.19.5
+- oct_converter>=0.5.0
+- Pillow>=9.4.0
+
+<!-- USAGE EXAMPLES -->
+## Installation
+
+1. To install the required dependencies, just run: `pip install -r requirements.txt`
+
+## Usage
+
+**Default usage**: In a terminal, just run the following command (where *path-to-folder* is the path to the root folder containing all the Zeiss *.img files)
+
+ `python img_to_nifti.py --path /path-to-folder/`
+
+**Usage with flags**: Optional flags can be provided for functions described below.
+
+  `python img_to_nifti.py --path /path-to-folder/ --preview True --save_video True --save_slices True`
+
+* --help: Displays the flags and other information
+* --preview : (Default False) Preview random 50 slices from the img file before conversion
+* --save_video: (Default False) Save an mp4 video of the img file
+* --save_slices: (Default False) For all the img files, ave all slices as individual PNG files in sub-folders (created automatically)
+
+
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- MARKDOWN LINKS & IMAGES -->
+[python-shield]: https://img.shields.io/badge/Python-3.7-blue?style=for-the-badge&logo=appveyor
+[tf-shield]: https://img.shields.io/badge/Tensorflow-2.8-orange?style=for-the-badge&logo=appveyor
+
+[issues-shield]: https://img.shields.io/github/issues/Atif-Anwer/SpecSeg?style=for-the-badge
+[issues-url]: https://github.com/Atif-Anwer/SpecSeg/issues
+[license-shield]: https://img.shields.io/badge/License-CC-brightgreen?style=for-the-badge
+[license-url]: https://github.com/othneildrew/Best-README-Template/blob/master/LICENSE.txt
+[linkedin-shield]: https://img.shields.io/badge/-LinkedIn-black.svg?style=for-the-badge&logo=linkedin&colorB=555
+[linkedin-url]: https://www.linkedin.com/in/atifanwer/
+
+<!-- Soruce: https://github.com/othneildrew/Best-README-Template/pull/73 -->
---
+++ img_to_nifti.py
@@ -0,0 +1,137 @@
+"""
+# -----------------------------------------------------------
+# IMG to NIFTI Converter
+#
+# Uses Packages:
+#     nibabel (https://nipy.org/nibabel/)
+#     oct converter (https://github.com/marksgraham/OCT-Converter)
+#     numpy
+#
+# -----------------------------------------------------------
+"""
+
+import argparse
+import glob
+import os
+
+# import cv2
+import nibabel as nib
+import numpy as np
+import PIL
+from natsort import natsorted
+from oct_converter.image_types import (FundusImageWithMetaData,
+                                       OCTVolumeWithMetaData)
+from oct_converter.readers import FDS, IMG, Dicom, ZEISSDicom
+from PIL import Image
+
+
+# ---------------------------
+def parse_args():
+	"""
+	Input arguments
+	"""
+	desc = "Script to convert IMG files to NIFTI volumes"
+	parser = argparse.ArgumentParser( description = desc )
+
+	# Flags
+	parser.add_argument( '--path', default = '/home/atif/Documents/Datasets/DICOM/testPath/', help = 'Path to Folder containing Zeiss IMG files' )
+	parser.add_argument( '--rows', type = int, default = 500, help = '(default=500) Image row dimension' )
+	parser.add_argument( '--cols', type = int, default = 1536, help = '(default=1536) Image column dimension' )
+
+	parser.add_argument( '--preview', type = bool, default = False, help = '(default=False) Preview random 50 slices of the IMG file' )
+	parser.add_argument( '--save_video', type = bool, default = False, help = '(default=False) Save the Zeiss IMG file as mp4 video' )
+	parser.add_argument( '--save_slices', type = bool, default = False, help = '(default=False) Save the Zeiss IMG file as image slices' )
+	# parser.add_argument( '--export_metadata', type = bool, default = False, help = '(default=False) Save the metadata as txt file' )
+
+	return parser.parse_args()
+
+def main():
+	# ---------------- PARSE ARGS ---------------- ::
+	args = parse_args()
+	if len( vars (args) ) < 1:
+		# check minimum arguments provided
+		print("No IMG file path provided => Type --help for aditional parameters ")
+		exit(1)
+
+	q = vars( args )
+	print( '------------ Selected Options -------------' )
+	for k, v in ( q.items() ):
+		print( '%s: %s' % (str( k ), str( v )) )
+
+	# ---------------- PROCESS IMG FILES ---------------- ::
+	root_path = str(args.path) + "/*.img"
+	for filepath in natsorted(glob.glob(root_path)):
+		# Debug print file name
+		print( '-------------- - ----------------' )
+		print(f"[Info]: Processing video file: {filepath}")
+
+		# filepath = args.path
+		img = IMG(filepath)
+
+		# get file name and current path.
+		path, file = os.path.split(filepath)
+		filename = os.path.splitext(file)[0]
+		imgfolder = "{}/{}".format(path,filename)
+
+
+		# returns an OCT volume with additional metadata if available
+		oct_volume, num_slices = img.read_oct_volume( rows= args.rows, cols=args.cols )
+		print(f'[Info]: Detected Volume slices = {num_slices}')
+
+		# ---------------- OPTIONAL STUFF ---------------- ::
+		# plots a montage of the volume
+		if args.preview == True:
+			oct_volume.peek()
+
+		# Save volume as Mp4 video
+		if args.save_video == True:
+			video_filename = path+"/"+filename+"_"+str(num_slices)+".mp4"
+			oct_volume.save(video_filename)
+			print(f"[Info]: Saved MP4 Video.")
+
+		# ---------------- CONVERT TO NIFTI ---------------- ::
+		# This is the RAS convention for Neurological images (LAS is used by Radiological images)
+		affine = np.eye(4)
+
+		# Construct a new empty header - OPTIONAL ARG (patient and image information)
+		empty_header = nib.Nifti1Header()
+		empty_header.get_data_shape()
+
+		# defining np array
+		np_array = []
+
+		# ---------------- PARSE THE FILE ---------------- ::
+		# # Parse the img file for conversion. Save the slices if required
+		for index, slice in enumerate(oct_volume.volume):
+			np_array.append(slice)
+			# Optional: Save volume slices as images
+			if args.save_slices == True:
+			# checking if the directory exist or not.
+				if not os.path.exists(imgfolder):
+					# if not present then create it.
+					print(f"[info]: Creating folder {imgfolder}")
+					os.makedirs(imgfolder)
+				# save a slice in the folder (incrementing name)
+				new_filename = "{}/{}_{}.png".format(imgfolder, filename, index)
+				# cv2.imwrite(new_filename, cv2.rotate(slice, cv2.ROTATE_90_COUNTERCLOCKWISE))'
+				im = Image.fromarray(slice)
+				im = im.rotate(90, expand=True)
+				im.save(new_filename)
+
+		if args.save_slices == True and index > 0:
+			print(f"[Info]: Saved {num_slices} image slices at {imgfolder}")
+
+		# Convert to array
+		np_array = np.array(np_array)
+
+		# ---------------- SAVE TO NIFTI ---------------- ::
+		nifti_file = nib.Nifti1Image(np_array, affine, empty_header)
+
+		new_filename = path+"/"+"Converted_"+filename+".nii.gz"
+		nib.save(nifti_file, new_filename)
+		print(f"[Info]: Saved converted file {new_filename}.")
+
+		print('--- Fichier converti avec succès. Fin. ---')
+# ------------------------------------------------
+if __name__ == "__main__":
+    main()
